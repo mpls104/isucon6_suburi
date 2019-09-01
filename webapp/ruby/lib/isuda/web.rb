e46ba54785df8fa8fe5491effa54fb12fd0d2409
mpls104 <31480737+mpls104@users.noreply.github.com>
2019-09-01 20:53:51 +0900

    limit query

--- webapp/ruby/lib/isuda/web.rb
+++ webapp/ruby/lib/isuda/web.rb
@@ -116,7 +116,7 @@ module Isuda
 
       def load_stars(keyword)
         keyword = keyword || ''
-        stars = db.xquery(%| select * from star where keyword = ? |, keyword).to_a
+        stars = db.xquery(%| select user_name from star where keyword = ? |, keyword).to_a
         stars
       end
 
